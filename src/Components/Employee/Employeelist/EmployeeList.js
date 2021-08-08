0deaa401cb19d463e4f4dec8cedf02d874b60808
anup-paul <anupdesktopit@gmail.com>
2021-08-08 23:13:47 +0600

    update

--- src/Components/Employee/Employeelist/EmployeeList.js
+++ src/Components/Employee/Employeelist/EmployeeList.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const EmployeeList = () => {
+
+    
+    let history = useHistory()
+
+    const handleAddEmployee = (e) => {
+        e.preventDefault();
+        history.push("/addEmployee")
+        console.log("clicked");
+    }
+
     return (
         <div className="d-flex justify-content-center" >
             <div className="row w-75 mt-3" >
-
+                <div className="d-flex justify-content-between" >
+                    <form class="d-flex">
+                        <input class="form-control w-75 me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-primary" type="submit">Search</button>
+                    </form>
+                    <button
+                        className="btn btn-outline-primary"
+                        onClick={(e)=>handleAddEmployee(e)}
+                    > <FontAwesomeIcon icon={faPlus} /> Add Employee     
+                    </button>
+                </div>
                 <table className="table mt-3">
                     <thead className="table-primary" >
                         <tr>
@@ -16,7 +37,7 @@ const EmployeeList = () => {
                             <th scope="col">Employee ID</th>
                             <th scope="col">Phone Number</th>
                             <th scope="col">Email</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">Action </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -27,7 +48,14 @@ const EmployeeList = () => {
                             <td>@mdo</td>
                             <td>@mdo</td>
                             <td>@mdo</td>
-                            <td>@mdo</td>
+                            <td>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Action Type</option>
+                                    <option value="1">Edit</option>
+                                    <option value="2">Suspend</option>
+                                    <option value="3">Former</option>
+                                </select>
+                            </td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
@@ -36,7 +64,14 @@ const EmployeeList = () => {
                             <td>@fat</td>
                             <td>@mdo</td>
                             <td>@mdo</td>
-                            <td>@mdo</td>
+                            <td>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Action Type</option>
+                                    <option value="1">Edit</option>
+                                    <option value="2">Suspend</option>
+                                    <option value="3">Former</option>
+                                </select>
+                            </td>
                         </tr>
 
 
--- src/Components/Home/Dashboard/Dashboard.js
+++ src/Components/Home/Dashboard/Dashboard.js
@@ -4,59 +4,59 @@ import { Link } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div>
-            <nav className ="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
-                <div className ="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
+                <div className="container-fluid">
 
-                    <div className ="collapse navbar-collapse" id="navbarNav">
-                        <ul className ="navbar-nav">
-                            <li className ="nav-item">
-                                <Link to="/Home" className ="nav-link active" aria-current="page" ><b>Home</b></Link>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/Home" className="nav-link active" aria-current="page" ><b>Home</b></Link>
                             </li>
-                            <li className ="nav-item dropdown">
-                                <Link className ="nav-link dropdown-toggle active" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><b>Employee</b></Link>
-                                <ul className ="dropdown-menu btn btn-primary">
-                                    <li><Link to="/addEmployee" className ="dropdown-item" >Add Employee</Link></li>
-                                    <li><Link to="/employees" className ="dropdown-item" >Employee List</Link></li>
-                                    <li><Link  className ="dropdown-item" >Suspended Employee list</Link></li>
-                                    <li><hr class="dropdown-divider"/></li>
-                                    <li><Link className ="dropdown-item" >Make Team</Link></li>
-                                    <li><Link to="/teamList" className ="dropdown-item" >Team List</Link></li>
-                                    <li><Link to="/addTeam" className ="dropdown-item" >Add Team</Link></li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle active" data-bs-toggle="dropdown" role="button" aria-expanded="false"><b>Employee</b></Link>
+                                <ul className="dropdown-menu btn btn-primary">
+                                    <li><Link to="/employees" className="dropdown-item" >Employee List</Link></li>
+                                    <li><Link to="/addEmployee" className="dropdown-item" >Add Employee</Link></li>
+                                    <li><Link className="dropdown-item" >Suspended Employee</Link></li>
+                                    <li><Link className="dropdown-item" >Former Employee</Link></li>
+                                    <li><hr class="dropdown-divider" /></li>
+                                    <li><Link to="/teamList" className="dropdown-item" >Team List</Link></li>
+                                    <li><Link to="/addTeam" className="dropdown-item" >Make Team</Link></li>
                                 </ul>
                             </li>
-                            
-                            <li className ="nav-item dropdown">
-                                <Link className ="nav-link dropdown-toggle active" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><b>Project</b></Link>
-                                <ul className ="dropdown-menu btn btn-primary">
-                                    <li><Link to="/projectCategoryList" className ="dropdown-item" >Project Category list</Link></li>
-                                    <li><Link to="/addProjectCategory" className ="dropdown-item" >Add Project Category</Link></li>
-                                    <li><Link to="/projects" className ="dropdown-item" >Project List</Link></li>
-                                    <li><Link to="/addProject" className ="dropdown-item" >Add Project</Link></li>
-                                    <li><Link className ="dropdown-item" >Archive Project List</Link></li>
-                                    
+
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle active" data-bs-toggle="dropdown" role="button" aria-expanded="false"><b>Project</b></Link>
+                                <ul className="dropdown-menu btn btn-primary">
+                                    <li><Link to="/projectCategoryList" className="dropdown-item" >Project Category list</Link></li>
+                                    <li><Link to="/addProjectCategory" className="dropdown-item" >Add Project Category</Link></li>
+                                    <li><Link to="/projects" className="dropdown-item" >Project List</Link></li>
+                                    <li><Link to="/addProject" className="dropdown-item" >Add Project</Link></li>
+                                    <li><Link className="dropdown-item" >Archive Project List</Link></li>
+
                                 </ul>
                             </li>
-                            
-                            <li className ="nav-item dropdown">
-                                <Link className ="nav-link dropdown-toggle active" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><b>Task</b></Link>
-                                <ul className ="dropdown-menu btn btn-primary">
-                                    <li><Link to="/addTask" className ="dropdown-item" >Add Task</Link></li>
-                                    <li><Link to="/taskList" className ="dropdown-item" >Task List</Link></li>
-                                    <li><Link  className ="dropdown-item" >Temporary Task</Link></li>
-                                    <li><Link  className ="dropdown-item" >Permanent Task</Link></li>
-                                     
+
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle active" data-bs-toggle="dropdown" role="button" aria-expanded="false"><b>Task</b></Link>
+                                <ul className="dropdown-menu btn btn-primary">
+                                    <li><Link to="/addTask" className="dropdown-item" >Add Task</Link></li>
+                                    <li><Link to="/taskList" className="dropdown-item" >Task List</Link></li>
+                                    <li><Link className="dropdown-item" >Temporary Task</Link></li>
+                                    <li><Link className="dropdown-item" >Permanent Task</Link></li>
+
                                 </ul>
                             </li>
-                            
-                            
-                            <li className ="nav-item dropdown">
-                                <Link className ="nav-link dropdown-toggle active" data-bs-toggle="dropdown"  role="button" aria-expanded="false"><b>Client</b></Link>
-                                <ul className ="dropdown-menu btn btn-primary">
-                                    <li><Link to="/clients" className ="dropdown-item" >Client List</Link></li>
-                                    <li><Link to="/addClient" className ="dropdown-item" >Add Client</Link></li>  
+
+
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle active" data-bs-toggle="dropdown" role="button" aria-expanded="false"><b>Client</b></Link>
+                                <ul className="dropdown-menu btn btn-primary">
+                                    <li><Link to="/clients" className="dropdown-item" >Client List</Link></li>
+                                    <li><Link to="/addClient" className="dropdown-item" >Add Client</Link></li>
                                 </ul>
                             </li>
-                            
+
                         </ul>
                     </div>
                 </div>
